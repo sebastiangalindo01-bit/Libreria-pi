fb96f80aa17d06e07a240973548f7decdedb2dbc
Sebastian Galindo Mejia <sebastian.galindo01@uceva.edu.co>
2026-06-02 22:45:13 -0500

    Corrección de main
    Corrección del main se agrego la librería y entrada de datos por teclado.

--- main.c
+++ main.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 #include "libproyecto.h"
 
-int main() {
-    struct Presupuesto p;
+int main(void) {
+    int duracion_meses;
+    int tamano_equipo;
+    double salario_promedio_mensual;
+    double infraestructura_mensual;
+    double licencias_mensuales;
+    double contingencia_porcentaje;
+    double resultado;
 
-    calcular_presupuesto(6, 5, 2000, 500, 300, 10, &p);
+    printf("Estimacion de Presupuestos\n\n");
 
-    printf("Total: %.2f\n", p.totalPresupuesto);
+
+    printf("Duracion (meses): ");
+    scanf("%d", &duracion_meses);
+
+    printf("Tamano del equipo: ");
+    scanf("%d", &tamano_equipo);
+
+    printf("Salario promedio mensual ($): ");
+    scanf("%lf", &salario_promedio_mensual);
+
+    printf("Infraestructura mensual ($): ");
+    scanf("%lf", &infraestructura_mensual);
+
+    printf("Licencias mensuales ($): ");
+    scanf("%lf", &licencias_mensuales);
+
+    printf("Contingencia (%%): ");
+    scanf("%lf", &contingencia_porcentaje);
+
+    resultado = calcular_presupuesto(duracion_meses, tamano_equipo, salario_promedio_mensual,
+                         infraestructura_mensual, licencias_mensuales,
+                         contingencia_porcentaje);
+
+    printf("\nTOTAL PRESUPUESTO: $%.2lf\n", resultado);
 
     return 0;
 }
